970935e3bcd71fe5f4cb84aa9d262d46dff31104
Ezekiel Ulrich <ezekielulrich@gmail.com>
2025-08-12 15:32:01 -0400

    Backup

--- build.sh
+++ build.sh
@@ -8,5 +8,5 @@ docker buildx create --use
 docker buildx build --platform linux/amd64,linux/arm64 \
     -t $IMAGE_NAME:latest --push .
 
-echo "Running Docker container at localhost:443"
+echo "Running Docker container at localhost:80"
 docker run --rm -it -p 80:80 -p 443:443 $IMAGE_NAME
