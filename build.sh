1faed90a25f587ff0f59f687bacf42e0e14d31be
Ezekiel Ulrich <ezekielulrich@gmail.com>
2025-08-12 15:09:00 -0400

    Backup

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+IMAGE_NAME="zkeulr/duelgood"
+
 git add .
 git commit -m "Backup"
 docker buildx create --use
 docker buildx build --platform linux/amd64,linux/arm64 \
-    -t zkeulr/duelgood:latest --push .
+    -t $IMAGE_NAME:latest --push .
 
 echo "Running Docker container at localhost:443"
 docker run --rm -it -p 80:80 -p 443:443 $IMAGE_NAME
