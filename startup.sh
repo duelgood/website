e6e9f40582cc8718d55dd6658fe68218a54178cc
Ezekiel Ulrich <ezekielulrich@gmail.com>
2025-08-22 13:55:42 -0400

    Update 20250822-135542

--- startup.sh
+++ startup.sh
@@ -20,8 +20,7 @@ configure_firewall() {
 }
 
 deploy_stack() {
-  sudo docker stop $(sudo docker ps -q) || true
-  sudo docker rm $(sudo docker ps -a -q) || true
+  sudo docker compose down --volumes --remove-orphans
   sudo docker compose pull
   sudo docker compose up -d
   sudo docker compose exec backend flask db-init || true
