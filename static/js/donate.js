c8694b7bbdbdf77230b7034e73c002b7d95ce4b7
Ezekiel Ulrich <ezekielulrich@gmail.com>
2025-09-14 18:24:41 -0400

    Update 20250914-182441

--- static/js/donate.js
+++ static/js/donate.js
@@ -89,7 +89,7 @@ document.addEventListener("DOMContentLoaded", async function () {
       }
 
       // 3. Confirm payment with redirect
-      const { error: stripeError } = await stripe.confirmPayment({
+      const { error: stripeError } = await stripe.confirmSetup({
         elements,
         confirmParams: {
           return_url: "https://duelgood.org/thank-you",
