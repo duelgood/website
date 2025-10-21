fdbb5586c188eb332c1dcf503a6a5b35712295c0
Ezekiel Ulrich <ezekielulrich@gmail.com>
2025-10-21 19:03:05 -0400

    Update 20251021-190304

--- static/js/index.js
+++ static/js/index.js
@@ -308,6 +308,8 @@ document.addEventListener("DOMContentLoaded", function () {
       return;
     }
 
+    donors.reverse();
+
     const ul = document.createElement("ul");
     ul.style.listStyle = "none";
     ul.style.padding = "0";
